bc2527b2382deb534a3831db4d13cd74479b2671
ottojolanki <ojolanki@stanford.edu>
2023-08-18 11:59:12 -0700

    REGD-112-shared-infrastructure

---
+++ src/regulome_infrastructure/regulome_dev/domain.py
@@ -0,0 +1,24 @@
+from constructs import Construct
+
+from aws_cdk.aws_certificatemanager import Certificate
+from aws_cdk.aws_route53 import HostedZone
+
+from typing import Any
+
+
+class Domain(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.name = 'dev.regulomedb.org'
+        self.certificate = Certificate.from_certificate_arn(
+            self,
+            'DomainCertificate',
+            'arn:aws:acm:us-west-2:920073238245:certificate/42fc15e0-50ad-43c3-815b-cd2419ac67c2',
+        )
+        self.zone = HostedZone.from_lookup(
+            self,
+            'DomainZone',
+            domain_name=self.name,
+        )
+
